615a9d3df46b3bd4def4a07f40fd84fb9653d59c
notforyou23 <notforyou23@gmail.com>
2025-05-20 17:01:17 -0400

    Refactor app into modules

---
+++ scripts/config.js
@@ -0,0 +1,12 @@
+export const CONFIG = {
+    APPLICATION_KEY: "5B39045E3312D99FE79E167EF56FB0E0",
+    API_KEY: "58b67f54-13ce-4cdc-94fa-e5bf91eb4c30",
+    MAC: "D4:8A:FC:82:38:47"
+};
+
+export const SAUNA_CONFIG = {
+    API_URL: 'https://api.huum.eu/action/home',
+    USERNAME: 'notforyou23@gmail.com',
+    PASSWORD: 'ofDYxD',
+    UPDATE_INTERVAL: 50000 // 50 seconds
+};
